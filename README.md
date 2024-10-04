24b4890dc16fc6bfbe02dfc6d6364f3edcb7d62d
Lindsay Elliott <lellio04@uoguelph.ca>
2024-10-04 12:04:26 -0400

    the recursion is working

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
 # ph-coding-challenge
 PH Coding Challenge
 
-testing
+This is a python microservice that is able to 
+- accept and store an uploaded DICOM file, extract
+- return any DICOM header attribute based on a DICOM Tag as a query parameter
+- finally convert the file into a PNG for browser-based viewing.
+
+
+Please design a RESTful API that exposes an interface for the functionality described above.
+
+Testing: Drop your DICOM file in the test_files folder
+
+
+How to run:
+
+In project directory:
+`source myenv/bin/activate`
+
+In virtual environment:
+`python app.py`
+
+
+
+http://127.0.0.1:5000/?tag=00041130
+`(0004, 1130) File-set ID                         CS: 'DicomDir'` 
+
+http://127.0.0.1:5000/?tag=00100010 
+`(0010, 0010) Patient's Name                      PN: 'NAYYAR^HARSH'`
+
--- app.py
+++ app.py
@@ -1,15 +1,56 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import pydicom
+from pydicom.data import get_testdata_file
+from pydicom.fileset import FileSet
+import os
 
 app = Flask(__name__)
 
+# Global variable to store the DICOM data
+dicom_data = None
+
+def find_tag(tag, ds, indent=""):
+    # Check if the tag is in the dataset and print the result
+    if tag in ds:
+        value = ds.get(tag)
+        return value
+
+    # Loop through all elements in the dataset
+    for elem in ds:
+        # If the element is a sequence (VR == "SQ"), recurse on each item in the sequence
+        if elem.VR == "SQ":
+            #print(indent + f"Checking sequence: {elem}")
+            for item in elem:
+                # Recursively call find_tag and capture its return value
+                result = find_tag(tag, item, indent + "    ")
+                # If a result is found, return it
+                if result != -1:
+                    return result
+    return -1
+
 @app.route('/')
 def home():
+    # Path to DICOM file
+    path = "test_files/XRAY/DICOMDIR"
+
+    # Get the DICOM tag requested in the query parameter 'tag'
+    tag = request.args.get('tag')
+
+    # A File-set can be loaded from the path to its DICOMDIR dataset
+    dicom_data = pydicom.dcmread(path)
+    print (dicom_data)
+
+    try:
+        # Try to convert the tag into an integer (DICOM tags are integers)
+        tag_int = int(tag, 16)  # Tags are often in hexadecimal
+        value = find_tag(tag_int, dicom_data, indent="")
+        print(value)        
+
+    except Exception as e:
+                return f"Error reading DICOM file: {e}", 400
+   
     return "Hello, Flask!"
 
-
-@app.route('/test')
-def testing():
-    return "Hello, Flask! testing route"
 
 if __name__ == '__main__':
     app.run(debug=True)
