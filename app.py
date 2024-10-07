19106b04834189eb9b076858427e22d79bb8bc6d
Lindsay Elliott <lellio04@uoguelph.ca>
2024-10-07 12:14:35 -0400

    more adjustments to seperate out the endpoints

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, request
 import pydicom
 import matplotlib.pyplot as plt
 import matplotlib
+import os
 matplotlib.use('Agg')
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+EXPORT_FOLDER = './export'
+app.config['EXPORT_FOLDER'] = EXPORT_FOLDER
+
+# Ensure the upload folder exists
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 # Recursive function to find the tag
 def find_tag(tag, ds, indent=""):
@@ -26,30 +36,46 @@ def find_tag(tag, ds, indent=""):
                     return result
     return -1
 
-# Function to take in a DCM and generate a png and save that
-def convert_to_png(image_data):
-    image_data = image_data.pixel_array  
 
-    # Save the image to a file
-    plt.imshow(image_data, cmap='gray')
-    plt.title("DICOM Image")
-    plt.axis('off')
-    plt.savefig("output_image.png", bbox_inches='tight')
-    plt.close()  # Close the plot to free memory
+# Endpoint to upload DCM file - puts in /upload folder
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # If file isnt in body - throw error
+    if 'file' not in request.files:
+        return 'No file part', 400
+
+    file = request.files['file']
+
+    # If file is empty, throw error
+    if file.filename == '':
+        return 'No selected file', 400
+    
+    # Only accept (.dcm) files 
+    if file: # and file.filename.endswith('.dcm'):
+        filename = file.filename
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+        return f'File {filename} uploaded successfully', 200
+    
+    # Throw general error if any other issues
+    return "Error uploading file", 400
 
 
-@app.route('/')
-def home():
+# Get tag from file
+@app.route('/tag',  methods=['GET'])
+def get_tag():
     # Path to DICOM file
-    path = "test_files/XRAY/DICOMDIR"
-    image = "test_files/IM000001"
+    file_name = request.args.get('file_name')  # Assuming file_id or filename is passed
+    path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    
+    if not os.path.exists(path):
+        return 'File not found', 404
 
     # Get the DICOM tag requested in the query parameter 'tag'
     tag = request.args.get('tag')
 
     # Read DICOM dataset
     dicom_data = pydicom.dcmread(path)
-    image_data = pydicom.dcmread(image)
 
     try:
         # Convert the tag into an integer (DICOM tags are integers)
@@ -59,15 +85,53 @@ def home():
 
         if value == -1:
             return f"Tag not found in DICOM file: {tag}", 201
-        
-        # Call function to save as a png
-        convert_to_png(image_data)
 
     except Exception as e:
         return f"Error reading DICOM file: {e}", 400
    
     # This doesnt have name in it so wondering if that is needed
     return value.to_json()
+
+
+# Get get image from file and save as png
+@app.route('/image',  methods=['GET'])
+def get_png():
+    # Path to DICOM file
+    file_name = request.args.get('file_name')  # Assuming file_id or filename is passed
+    path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+    
+    if not os.path.exists(path):
+        return 'File not found', 404
+
+    # Read DICOM dataset
+    dicom_data = pydicom.dcmread(path)
+
+    try:
+        # render image
+        dicom_data = dicom_data.pixel_array  
+
+        # Save the image to a file
+        plt.imshow(dicom_data, cmap='gray')
+        plt.title("DICOM Image")
+        plt.axis('off')
+
+        # Create the folder if it doesn't exist
+        os.makedirs(app.config['EXPORT_FOLDER'], exist_ok=True)
+        path = os.path.join(app.config['EXPORT_FOLDER'], file_name)
+
+        plt.savefig(f"{path}.png", bbox_inches='tight')
+        plt.close()  # Close the plot to free memory
+
+    except Exception as e:
+        return f"Error converting DICOM file to png: {e}", 400
+    
+    return f'File {file_name} saved as png to export folder', 200
+
+
+# root
+@app.route('/')
+def home():
+    return "Root"
 
 
 if __name__ == '__main__':
