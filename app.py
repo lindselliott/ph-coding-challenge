4ac258cb870d5422fbe0a671a2b7b83633a85431
Lindsay Elliott <lellio04@uoguelph.ca>
2024-10-07 11:22:28 -0400

    generating the image

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import pydicom
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 
 app = Flask(__name__)
 
@@ -24,29 +26,30 @@ def find_tag(tag, ds, indent=""):
                     return result
     return -1
 
-def convert_to_png(dicom_data):
-    if 'PixelData' in dicom_data:
-        print("Pixel data found.")
-    else:
-        print("Pixel data not found in this DICOM file.")
+# Function to take in a DCM and generate a png and save that
+def convert_to_png(image_data):
+    image_data = image_data.pixel_array  
 
-    # new_image = dicom_data.pixel_array.astype(float)
-    # print(new_image)
-
-    # plt.imshow(dicom_data.pixel_array, cmap=plt.cm.gray)
-    # plt.show()
+    # Save the image to a file
+    plt.imshow(image_data, cmap='gray')
+    plt.title("DICOM Image")
+    plt.axis('off')
+    plt.savefig("output_image.png", bbox_inches='tight')
+    plt.close()  # Close the plot to free memory
 
 
 @app.route('/')
 def home():
     # Path to DICOM file
     path = "test_files/XRAY/DICOMDIR"
+    image = "test_files/IM000001"
 
     # Get the DICOM tag requested in the query parameter 'tag'
     tag = request.args.get('tag')
 
     # Read DICOM dataset
     dicom_data = pydicom.dcmread(path)
+    image_data = pydicom.dcmread(image)
 
     try:
         # Convert the tag into an integer (DICOM tags are integers)
@@ -57,8 +60,8 @@ def home():
         if value == -1:
             return f"Tag not found in DICOM file: {tag}", 201
         
-        # commenting this out as I was not able to test a DICOM with PixelData
-        # convert_to_png(dicom_data)
+        # Call function to save as a png
+        convert_to_png(image_data)
 
     except Exception as e:
         return f"Error reading DICOM file: {e}", 400
